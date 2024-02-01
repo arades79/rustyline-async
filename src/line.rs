77afffbf2a2c9666d6fa6eb3c2a6854144a230bc
Skyelar Craver <skyelar.craver@cellink.com>
2024-02-01 16:53:23 -0500

    cursor doesn't need to move

--- src/line.rs
+++ src/line.rs
@@ -174,9 +174,7 @@ impl LineState {
 	}
 	pub fn update_prompt(&mut self, prompt: String, term: &mut impl Write) -> Result<(), ReadlineError> {
 		self.clear(term)?;
-		let prompt_delta = (self.prompt.len() as isize) - (prompt.len() as isize);
 		self.prompt = prompt;
-		self.move_cursor(prompt_delta)?;
 		self.set_cursor(term)?;
 		self.render(term)?;
 		Ok(())
